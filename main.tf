a2a1c8fa08bfeb01a85334fb8b657fdac4c3fc43
mannem302 <102237426+mannem302@users.noreply.github.com>
2024-08-14 12:17:30 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,10 +2,10 @@ provider "aws" {
  region = "ap-south-1" 
 } 
 resource "aws_instance" "web" {
-  ami           = "ami-0c2af51e265bd5e0e "  # Replace with your desired AMI ID
+  ami           = "ami-0c2af51e265bd5e0e"  # Replace with your desired AMI ID
   instance_type = "t2.micro"               # Replace with your desired instance type
   key_name      = "Mumbai_New_Key"           # Replace with your key pair name
-  vpc_security_group_ids = ["sg-0406450f2837ba102"]   # your security group id to be replaced.
+  vpc_security_group_ids = ["sg-005f6e32c5fede8c5"]   # your security group id to be replaced.
   tags = {
     Name = "Prod_Server"
     Env = "Prod"
