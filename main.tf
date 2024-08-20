7e078da9abb7c70eb937c2e876d5551b1d499c40
mannem302 <102237426+mannem302@users.noreply.github.com>
2024-08-20 09:46:34 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
  region = "ap-south-1" 
 } 
 resource "aws_instance" "web" {
-  count         = 2
+  count         = 0
   ami           = "ami-0c2af51e265bd5e0e"  # Replace with your desired AMI ID
   instance_type = "t2.micro"               # Replace with your desired instance type
   key_name      = "Mumbai_New_Key"           # Replace with your key pair name
